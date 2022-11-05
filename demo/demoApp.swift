4aefc9e4645b2d0d54864d7657c00d9f9d9d975a
Wang Tong <wth7512@mail.com>
2022-11-05 21:29:06 +0800

    Initial Commit

---
+++ demo/demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  demoApp.swift
+//  demo
+//
+//  Created by Wangtong on 2022/11/5.
+//
+
+import SwiftUI
+
+@main
+struct demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
